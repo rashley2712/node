36d126729c3ed10ef0de9b538b94d0dd0e63aab0
Richard Ashley <rashley@gmail.com>
2015-05-27 16:12:57 +0200

    Keypresses

--- ws_server.js
+++ ws_server.js
@@ -38,6 +38,8 @@ wsServer.on('request', function(request) {
 			if(clients[i].connection == connection) {
 				console.log(timeStamp() + connection.remoteAddress + " (" + clients[i].username + ") disconnected.");
 				clients.splice(i, 1);
+				informViewers();
+				
 			}
 		}
     });
@@ -57,10 +59,60 @@ function uniqueUsername(username) {
 	return true;
 }
 
+function getConnectionForUsername(username) {
+	for (i in clients) {
+		if (clients[i].username == username) return clients[i].connection;
+	}
+	return false;
+}
+
+function getViewersOnly() {
+	clientShortList = []
+	for (i in clients) {
+		if (clients[i].username == username) clientShortList.push(clients[i]);
+	}
+	return clientShortList;
+	
+}
+
+function informViewers() {
+	// Informs all viewers of an update to the user list
+	console.log("Updating the viewers of a change to the list of users.");
+	var clientdata = [];
+	for (i in clients) {
+		var clientObject = {username: null, status: null, ping: null};
+		clientObject.username = clients[i].username;
+		clientObject.ping = clients[i].ping;
+		clientObject.status = clients[i].status;
+		console.log(clientObject);
+		clientdata.push(clientObject);
+	}
+	console.log("Updated user list...");
+	console.log(clientdata);
+	JSONpacket = JSON.stringify({msg: "users", data: clientdata});
+	for (i in clients) {
+		connection = clients[i].connection;
+		connection.send(JSONpacket);
+	}
+}
+
+function informViewersOfKeyPress(player, direction, status) {
+	// Informs all viewers of a keypress from a player
+	clientdata = {player: player, direction: direction, status: status};
+	
+	console.log(clientdata);
+	JSONpacket = JSON.stringify({msg: "keypress", data: clientdata});
+	clientShortList = getViewersOnly();
+	for (i in clientShortList) {
+		connection = clientShortList[i].connection;
+		connection.send(JSONpacket);
+	}
+}
 
 function handleMessage(mString, connection) {
 	var pMessage = mString.split("|");
 	var command = pMessage[0];
+	// console.log("Raw message: " + mString);
 	console.log("Command: " + command);
 	if (command=="connect") {
 		username = pMessage[1];
@@ -74,7 +126,9 @@ function handleMessage(mString, connection) {
 			clientData.status = 0;
 			clients.push(clientData);
 			connection.sendUTF(JSON.stringify({msg: "connected", data: "You are connected as user: " + username}));
-			console.log(timeStamp() + "New user connected [" + id + "] " + username);
+			console.log(timeStamp() + "New user connected [" + id + "] " + username);			
+			informViewers();
+		
 		} else {
 			connection.sendUTF(JSON.stringify({msg: "error", data: "Username (" + username + ") is already connected. Sorry."}));
 			connection.close();
@@ -95,6 +149,18 @@ function handleMessage(mString, connection) {
 			clientdata.push(clientObject);
 		}
 		connection.sendUTF(JSON.stringify({msg: "users", data: clientdata}));	
+	}
+
+	if (command=="keypress") {
+		direction = pMessage[1];
+		status = pMessage[2];
+		console.log("keypress received: " + direction + "[" + status + "]");
+		for (i in clients) {
+			if(clients[i].connection == connection) thisClient = clients[i];
+		}
+		player = thisClient.username;
+		
+		informViewersOfKeyPress(player, direction, status);
 	}
 
 	if (command=="ping") {
