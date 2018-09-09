a54d3cc7cf0ee192b46a9cd278e719029fbd901e
Ubuntu <ubuntu@ip-172-26-9-135.eu-west-2.compute.internal>
2018-09-09 17:04:10 +0000

    added nodeserver

---
+++ nodeserver.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+/** nodeserver.js  */
+
+'use strict'
+
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
+
+var port = process.argv[2]
+
+if (port == undefined) port = 80
+
+var server = http.createServer(function (request, response) {
+	// request handling logic
+	var ip = request.socket.remoteAddress;
+  // console.log("Request received from: " + ip)
+	// console.log("URL: " + req.url)
+	var URLData = url.parse(request.url, true)
+	// console.log(URLData)
+	var parts = URLData.pathname.split('/')
+	console.log("Requesting: " + parts)
+	switch(parts[1]) {
+		case 'status' :
+			writeout(null, "OK");
+			break;
+		default :
+			console.log("Received request for a file: " + request.url);
+			var filename = URLData.pathname.substring(1);
+			console.log("Getting file: " + filename);
+			fileServer(filename, response);
+		}
+
+	function fileServer(filename, response) {
+		var rootPath = "/var/www/";
+		if (filename=="") { filename = "index.html"};
+		var fullFilename = rootPath + filename
+		var contentType = 'text/html';
+		var extname = path.extname(filename);
+		var contentEncoding = 'utf8';
+		switch (extname) {
+			case '.js':
+					contentType = 'text/javascript';
+					break;
+			case '.css':
+					contentType = 'text/css';
+					break;
+			case '.json':
+					contentType = 'application/json';
+					break;
+			case '.png':
+					contentType = 'image/png';
+					break;
+			case '.gif':
+					contentType = 'image/gif';
+					break;
+			case '.jpg':
+					contentType = 'image/jpg';
+					break;
+			case '.ttf':
+					contentType = 'application/x-font-ttf';
+					break;
+			case '.fit':
+			case '.fits':
+					contentType = 'application/fits';
+					break;					
+			case '.gz':
+					contentType = 'application/json';
+					contentEncoding = 'gzip';
+ 					console.log("Serving a zip file.")
+					break;
+			}
+
+			fs.readFile(fullFilename, function(error, content) {
+			response.writeHead(200, { 'Content-Type': contentType, 'Content-Encoding': contentEncoding });
+			//if (content!=null) console.log(content.toString())
+		  response.end(content, 'utf-8');
+		})
+
+	}
+
+	function writeout(err, data) {
+		response.writeHead(200, { 'Content-Type': 'application/json',
+							  'Access-Control-Allow-Origin': '*' })
+		response.write(data)
+		response.end()
+	}
+
+
+	}).on('error', function(err) { console.error(err)})
+
+server.listen(port)
+
+server.on('listening', function(socket) {
+	console.log("Listening on port: " + port)
+	})
